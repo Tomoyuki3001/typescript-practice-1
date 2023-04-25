b77f219f8d46dc649ed8556f1c4c97daf9027c42
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-04-25 11:33:32 -0700

    Added the human illustlation

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,7 @@
+export function HangmanWord() {
+    const word = "test"
+    return (<div style={{display: "flex", gap:".25em", fontSize:"6rem", fontWeight:"bold", textTransform:"uppercase", fontFamily:"monospace"}}>
+        
+    </div>
+    )
+}
