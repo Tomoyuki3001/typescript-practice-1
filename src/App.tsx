78e2ab55061b716c0bd0324f242d6e477c9ed3d8
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-05-04 09:14:52 -0700

    Added some codes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { HangmanDrawing } from "./HangmanDrawing"
 import { HangmanWord } from "./HangmanWord"
 import { Keyboard } from "./Keyboard"
 
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+  const [wordToGuess, setWordToGuess] = useState(getWord)
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
@@ -33,6 +35,7 @@ function App() {
 
       e.preventDefault()
       addGuessedLetter(key)
+      setWordToGuess(getWord())
     }
 
     document.removeEventListener('keypress', handler)
@@ -41,6 +44,23 @@ function App() {
       document.removeEventListener('keypress', handler)
     }
   },[guessedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if(key !== 'Enter') return
+
+      e.preventDefault()
+      setGuessedLetters([])
+      addGuessedLetter(getWord())
+    }
+
+    document.removeEventListener('keypress', handler)
+
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [])
 
   return <div style={{
     maxWidth: "800px",
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,14 +1,16 @@
 type HangmanWordProps = {
-    guessedLtters: string[],
+    guessedLtters: string[]
     wordToGuess: string
+    reveal?: boolean
 }
 
-export function HangmanWord({guessedLtters, wordToGuess}:
+export function HangmanWord({guessedLtters, wordToGuess, reveal=false}:
     HangmanWordProps) {
     return (<div style={{display: "flex", gap:".25em", fontSize:"6rem", fontWeight:"bold", textTransform:"uppercase", fontFamily:"monospace"}}>
         {wordToGuess.split('').map((letter, index) => (
             <span style={{borderBottom: '.1em solid black'}} key={index}>
-                <span style={{visibility: guessedLtters.includes(letter) ? 'visible' : 'hidden'}}>{letter}</span>
+                <span style={{visibility: guessedLtters.includes(letter) || reveal ? 'visible' : 'hidden',
+                color: !guessedLtters.includes(letter) && reveal ? 'red' : 'black',}}>{letter}</span>
                 </span>
         ))}
     </div>
